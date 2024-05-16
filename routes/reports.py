1d0818548e118ed204f1e653e423fbc9b02c79e2
BotLay <norma.madrigal@becarios.silent4business.com>
2024-05-16 14:47:44 -0600

    feat:querys extras

--- routes/reports.py
+++ routes/reports.py
@@ -10,18 +10,37 @@ reports = APIRouter()
 def getEmpleados():
     return {"message": "empleados"}
 
-# Corrected route name to match the SQL query route
-
-
-@reports.get('/getUsers', tags=["ReportsXls"])
+# Users #############
+@reports.get('/Users', tags=["ReportsXls"])
 def retrieve_all_item():
-    query = "SELECT * FROM ussers"
+    query = """
+            select  
+            e.name as Nombre, 
+            e.email as Correo_Electonico, 
+            r.title as Roles,
+            e.name as Empleado_Vinculado,
+            a.area as Area, 
+            p.puesto as Puesto
+
+            from empleados e  
+
+            inner join role_user ru on e.id=ru.user_id 
+            inner join roles r on ru.role_id =r.id
+            inner join areas a ON e.area_id=a.id
+            inner join puestos p ON e.puesto_id =p.id
+
+            where 
+            r.deleted_at IS null
+
+            order by 
+            name asc;
+        """
 
     resultados = ejecutar_consulta_sql(cursor, query)
     exportar_a_excel(resultados, "reportsfile/empleados/users.xlsx")
 
 
-# Corrected route name to match the SQL query route
+# Empleados
 @reports.get('/empleadosPuestos', tags=["ReportsXls"])
 def getEmpleadosPuestos():
     query = """
@@ -43,6 +62,112 @@ def getEmpleadosPuestos():
     resultados = ejecutar_consulta_sql(cursor, query)
     exportar_a_excel(
         resultados, "reportsfile/empleados/empleadosPuestos.xlsx")
+    
+## Puestos
+@reports.get('/Puestos', tags=["ReportsXls"])
+def getPuestos():
+    query = """
+            select 
+            p.puesto, a.area , a.descripcion 
+
+            from puestos p 
+            inner join 
+            areas a on p.id_area=a.id
+            
+            WHERE
+            p.deleted_at IS NULL; 
+        """
+    resultados = ejecutar_consulta_sql(cursor, query)
+    exportar_a_excel(
+        resultados, "reportsfile/empleados/Puestos.xlsx")
+    
+## Roles
+@reports.get('/Roles', tags=["ReportsXls"])
+def getRoles():
+    query = """
+            select r.id as ID, r.title as Nombre_del_rol
+            
+            from roles r 
+            WHERE
+            r.deleted_at IS NULL;
+        """
+    resultados = ejecutar_consulta_sql(cursor, query)
+    exportar_a_excel(
+        resultados, "reportsfile/empleados/Roles.xlsx")
+
+## Soporte
+@reports.get('/puestoAreaDesc', tags=["ReportsXls"])
+def getpuestoAreaDesc():
+    query = """
+             
+        """
+    resultados = ejecutar_consulta_sql(cursor, query)
+    exportar_a_excel(
+        resultados, "reportsfile/empleados/puestoAreaDesc.xlsx")
+
+#############extra
+@reports.get('/puestoAreaDesc', tags=["ReportsXls"])
+def getpuestoAreaDesc():
+    query = """
+             
+        """
+    resultados = ejecutar_consulta_sql(cursor, query)
+    exportar_a_excel(
+        resultados, "reportsfile/empleados/puestoAreaDesc.xlsx")
+
+#############extra
+@reports.get('/puestoAreaDesc', tags=["ReportsXls"])
+def getpuestoAreaDesc():
+    query = """
+             
+        """
+    resultados = ejecutar_consulta_sql(cursor, query)
+    exportar_a_excel(
+        resultados, "reportsfile/empleados/puestoAreaDesc.xlsx")
+
+#############extra
+@reports.get('/puestoAreaDesc', tags=["ReportsXls"])
+def getpuestoAreaDesc():
+    query = """
+            
+        """
+    resultados = ejecutar_consulta_sql(cursor, query)
+    exportar_a_excel(
+        resultados, "reportsfile/empleados/puestoAreaDesc.xlsx")
+
+#############extra
+@reports.get('/puestoAreaDesc', tags=["ReportsXls"])
+def getpuestoAreaDesc():
+    query = """
+             
+        """
+    resultados = ejecutar_consulta_sql(cursor, query)
+    exportar_a_excel(
+        resultados, "reportsfile/empleados/puestoAreaDesc.xlsx")
+
+#############extra
+@reports.get('/puestoAreaDesc', tags=["ReportsXls"])
+def getpuestoAreaDesc():
+    query = """
+            
+        """
+    resultados = ejecutar_consulta_sql(cursor, query)
+    exportar_a_excel(
+        resultados, "reportsfile/empleados/puestoAreaDesc.xlsx")
+
+#############extra
+@reports.get('/puestoAreaDesc', tags=["ReportsXls"])
+def getpuestoAreaDesc():
+    query = """
+             
+        """
+    resultados = ejecutar_consulta_sql(cursor, query)
+    exportar_a_excel(
+        resultados, "reportsfile/empleados/puestoAreaDesc.xlsx")
+
+
+
+
 
 
 def ejecutar_consulta_sql(cursor, consulta):
